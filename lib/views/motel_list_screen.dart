2b9dc47ca3e91c0be5d33744df658e35d7f82689
hpzynha <larissa.nogueira.rocha@gmail.com>
2025-02-05 14:50:01 -0400

    ajustes e dependencias

---
+++ lib/views/motel_list_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MotelListScreen extends StatelessWidget {
+  const MotelListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
